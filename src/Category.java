a2032b1468e71afe3e0dac08a35a2569367ea292
Sveta <vanyushina.svetlana@gmail.com>
2017-11-02 00:01:02 +0300

    Category class was added.

---
+++ src/Category.java
@@ -0,0 +1,14 @@
+
+
+
+public class Category {
+    private String categoryName;
+    private int amount;
+
+    public Category(String categoryName, int amount){
+        this.categoryName = categoryName;
+        this.amount = amount;
+    }
+
+
+}
