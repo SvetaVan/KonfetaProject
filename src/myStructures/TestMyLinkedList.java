cec3d5bcbb5af7111fe2ceaabd02d293c55b8fb3
Sveta <vanyushina.svetlana@gmail.com>
2017-11-26 23:36:45 +0300

    Добавлен класс MyLinkedList(реализован add, size) и тест

---
+++ src/myStructures/TestMyLinkedList.java
@@ -0,0 +1,30 @@
+package myStructures;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println(testMyLinkedList());
+    }
+
+    public static String testMyLinkedList (){
+        MyLinkedList <Integer> list = new MyLinkedList<>();
+        if (list.size()!=0){
+            return "при проверки на нулевое количество элементов - не возвращает 0";
+        }
+
+        list.add(2);
+
+        if (list.size()!=1){
+            return "если в коллекции один элемент - не возвращает размер 1";
+        }
+
+        list.add(2);
+        list.add(2);
+
+        if (list.size()!=3){
+            return "если в коллекции 3 элемента - не возвращает размер 3";
+        }
+        return "прошло проверку";
+
+    }
+
+}
